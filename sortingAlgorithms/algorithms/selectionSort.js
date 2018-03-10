a76c68ac05f4ed7b16d3e3cf45482c334a4fec07
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-03-09 18:18:46 -0800

    revise selectionSort algorithm to remove unnecessary variables and minimize looping

--- sortingAlgorithms/algorithms/selectionSort.js
+++ sortingAlgorithms/algorithms/selectionSort.js
@@ -1,14 +1,11 @@
 'use strict';
-// find the smallest number and swap it with the 0th element; then find 2nd smallest and swap with 1st element; continue until all are placed
-// [7,3,2,5,4,9]
-const selectionSort = (collection) => {
-  for(let n = 0; n < collection.length; n++){
 
-    for (let i = n; i < collection.length; i++){
+const selectionSort = (collection) => {
+  for(let n = 0; n < collection.length - 2; n++){
+    for (let i = n + 1; i < collection.length; i++){
       let currentSmallest = collection[n];
       let indexOfSmallest = n;
       if(collection[i] < currentSmallest){
-        currentSmallest = collection[i];
         indexOfSmallest = i;
       }
       if(indexOfSmallest !== n){
